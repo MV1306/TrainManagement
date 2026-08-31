e36e8058c492e8fdc62879287cb4d7f5a19bc93f
Manoj Prabakaran <manojprabakaran2001@gmail.com>
2026-08-31 22:02:05 +0530

    CORS handled

--- api/Program.cs
+++ api/Program.cs
@@ -3,25 +3,42 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpClient("erail", c =>
-{
-    c.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/124 Safari/537.36");
-    c.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,*/*");
-    c.DefaultRequestHeaders.Add("Referer", "https://erail.in/");
-    c.Timeout = TimeSpan.FromSeconds(15);
-});
+builder.Services.AddHttpClient(
+    "erail",
+    c =>
+    {
+        c.DefaultRequestHeaders.Add(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/124 Safari/537.36"
+        );
+        c.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,*/*");
+        c.DefaultRequestHeaders.Add("Referer", "https://erail.in/");
+        c.Timeout = TimeSpan.FromSeconds(15);
+    }
+);
 
-builder.Services.AddCors(options =>
-    options.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
-              .AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(
+    options =>
+        options.AddDefaultPolicy(
+            policy =>
+                policy
+                    .WithOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://192.168.29.141:10"
+                    )
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+        )
+);
 
 var app = builder.Build();
 
